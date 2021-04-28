9b5599ff3157a0a70609d75424560e851f00a4eb
Glitch (shocking-melon-raclette) <none>
2021-04-28 14:13:02 +0000

    🧀🚓 Checkpoint ./DarkMan.js:17585666/2155

--- DarkMan.js
+++ DarkMan.js
@@ -72,15 +72,18 @@ DarkMan.on("guildMemberAdd", member => {
   let DarkMan = member.user.avatarURL();
   if (!channel) return;
   const joinembed = new Discord.MessageEmbed()
+   
     .setTitle(``)
+    .setDescription(`
+      > hello sir ${member} wellcome to
+      > __${member.guild.name}__ we hope you enjoy with us.`
+      )
     .setAuthor(`${member.guild.name}`, `https://cdn.discordapp.com/attachments/827541229734592522/836962152430043186/image0.gif`)
     .setColor("#303135") 
+    .setImage("https://cdn.discordapp.com/attachments/701779288772444160/836964659831504946/image0.gif")
     .addField(
-      "wellcome",`
-      > hello sir ${member} wellcome to
-      > __${member.guild.name}__ we hope you enjoy with us.`)
-    .serImage("https://cdn.discordapp.com/attachments/701779288772444160/836964659831504946/image0.gif")
-  
+      "click here to see MrFox website",`__[Mr.Fox](https://mrfox.ga/)__`)
+    
   channel.send(joinembed);
 });
 
